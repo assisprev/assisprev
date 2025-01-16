fc06c82d10c52fe0913e3d6cc1cc5153b4226acd
Esteban Gualpa <esteban.gualpa@assisprev.com.ec>
2025-01-16 11:21:58 -0500

    Primer commit

---
+++ colaborador.js
@@ -0,0 +1,36 @@
+const empleados = [
+    {
+        id: "colaborador2025",
+        nombre: "Sebastain Gualpa ",
+        puesto: "Desarrollador de software",
+        ubicacion: "Quito,Ecuador",
+        foto: "https://th.bing.com/th/id/R.bf5ed4552b3eb973404418258fdff76c?rik=OUpi18ylKjWfrw&pid=ImgRaw&r=0",
+        whatsapp: "5930999266186",
+        telefono: "+5930999266186",
+        email: "esteban.gualpa@smarthelpit.com",
+        movil: "+5930123456789",
+    },
+    {
+        id: "colaborador01",
+        nombre: "María Bertila Aguirre Lapo",
+        puesto: "Auxiliar de Limpieza",
+        ubicacion: "Quito,Ecuador",
+        foto: "Auxiliar de limpieza/MARIA AGUIRRE.JPG",
+        whatsapp: "5930981352355",
+        telefono: "+59023824600",
+        email: "maria.aguirre@assisprev.com.ec",
+        movil: "+5930981352355",
+    },
+    {
+        id: "colaborador02",
+        nombre: "Ketty Mabel Arana Macias",
+        puesto: "Asesor Comercial",
+        ubicacion: "Guayaquil,Ecuador",
+        foto: "Asesores Comerciales GYE/KETTY ARANA.png",
+        whatsapp: "5930981352355",
+        telefono: "+59023824600",
+        email: "maria.aguirre@assisprev.com.ec",
+        movil: "+5930981352355",
+    },
+
+];
